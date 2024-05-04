3336f4d6aa4372cb9e559a017a1e6880077d7de7
sunyk <2876053639@qq.com>
2024-05-05 02:02:07 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="cesiumContainer"></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from "vue";
+import * as Cesium from "cesium";
+
+onMounted(() => {
+  let viewer = new Cesium.Viewer("cesiumContainer");
+  console.log(viewer)
+});
+</script>
+
+<style scoped>
+#cesiumContainer {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
